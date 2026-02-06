7630b778ddd9e2eb3c64216b9414c333907b58ea
ShyamRaj98 <shyamdeepu1998@gmail.com>
2026-02-06 23:57:26 +0530

    api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: API_BASE + "/api",
 });
 
 api.interceptors.request.use((config) => {
