432ae4fa95b69be541a78452c1002b863f7eb3de
ShyamRaj98 <shyamdeepu1998@gmail.com>
2026-02-07 12:11:21 +0530

    cors upload

--- server/app.js
+++ server/app.js
@@ -17,6 +17,8 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://fixmyareas.netlify.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
